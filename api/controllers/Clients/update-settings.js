e4fe71bb25d04e54f73fe29697448a4e869596b8
kum83 <momo821123@outlook.com>
2021-08-02 10:18:18 +0300

    save vimeo domain

--- api/controllers/Clients/update-settings.js
+++ api/controllers/Clients/update-settings.js
@@ -1,6 +1,12 @@
 module.exports = {
 
   friendlyName: 'Update client settings',
+  inputs: {
+    id: {
+      type: 'number',
+      required: true,
+    },
+  },
 
   exits: {
     badRequest: {
@@ -9,8 +15,11 @@ module.exports = {
   },
 
   fn: async function(inputs, exits) {
-
-    const response = await sails.helpers.clientSettings.update(this.req.client.id, this.req.body)
+    console.log("this.req.client = ", this.req.client); 
+    console.log("inputs.clientId = ", inputs.id);
+    const clientId = this.req.client.id ? this.req.client.id: inputs.id;
+    console.log("clientId = ", clientId);
+    const response = await sails.helpers.clientSettings.update(clientId, this.req.body)
       .tolerate('invalidKeys', async (e) => {
         exits.badRequest(e.message)
         return e
