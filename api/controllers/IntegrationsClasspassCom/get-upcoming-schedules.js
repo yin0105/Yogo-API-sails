5dc941f751be5d17c277b73da623d6194a549fb3
kum83 <momo821123@outlook.com>
2021-08-05 10:25:15 +0300

    f

--- api/controllers/IntegrationsClasspassCom/get-upcoming-schedules.js
+++ api/controllers/IntegrationsClasspassCom/get-upcoming-schedules.js
@@ -13,7 +13,6 @@ module.exports = async (req, res) => {
   if (!page_size) return res.badRequest("Missing query 'page_size'");
   if (!start_date) return res.badRequest("Missing query 'start_date'");
   if (!end_date) return res.badRequest("Missing query 'end_date'");
-  if (!client) return res.badRequest("Invalid partner_id"); 
 
   const client = await Client.findOne({id: partner_id});
   if (!client) return res.badRequest("Invalid partner_id");
@@ -21,94 +20,69 @@ module.exports = async (req, res) => {
   const venue = await Branch.findOne({client: partner_id, id: venue_id});
   if (!venue) return res.badRequest("Invalid venue_id");
 /*
-SELECT class.id AS schedule_id, CONCAT(class.date, 'T', class.`start_time`) AS start_datetime, CONCAT(class.date, 'T', class.`end_time`) AS end_datetime, class_type.id AS class_type_id,
- class_type.name AS class_type_name, class_type.description AS class_type_desc, class_type.updatedAt AS class_type_last_updated, room.id AS room_id, room.name AS room_name, 
- room.updatedAt AS room_last_updated, class.seats AS total_spots,   
+SELECT , , , ,
+ , , , , , 
+ , ,   
  
  FROM ((class LEFT JOIN room ON class.room=room.id) 
  LEFT JOIN branch ON room.branch=branch.id) 
  LEFT JOIN class_type ON class.class_type=class_type.id 
  WHERE 
- class.client=1 AND branch.id=1 AND DATE BETWEEN '2021-06-01' AND '2022-01-01'
+ c.client=1 AND b.id=1 AND DATE BETWEEN '2021-06-01' AND '2022-01-01'
 */
   const schedules = await knex({c: 'class'})
   .leftJoin({r: 'room'}, 'r.id', 'c.room')
   .leftJoin({b: 'branch'}, 'b.id', 'r.branch')
   .leftJoin({ct: 'class_type'}, 'ct.id', 'c.class_type')
-  .innerJoin({oi: 'order_item'}, 'o.id', 'oi.order')
   .select(
-      knex.raw('CAST(DATE_ADD("1970-01-01", INTERVAL o.paid/1000 SECOND) AS DATE) AS dd'), 
-      knex.raw('DATE_ADD("1970-01-01", INTERVAL FLOOR(o.paid/1000) SECOND) AS paid'), 
-      knex.raw('o.invoice_id AS invoice_id'),
-      knex.raw('o.user AS user_id'),
-      knex.raw('o.non_user_name AS non_user_name'),
-      knex.raw('o.non_user_email AS non_user_email'),
-      knex.raw('CONCAT(u.first_name, u.last_name) AS user_name'), 
-      knex.raw('u.email AS user_email'),
-      knex.raw('oi.name AS item_name'),
-      knex.raw('oi.item_type AS item_type'),
-      knex.raw('oi.item_id AS item_id'),
-      knex.raw('oi.count AS item_count'),
-      knex.raw('oi.item_price AS item_price'),
-      knex.raw('oi.total_price AS item_total_price'),
-      knex.raw('oi.vat_amount AS item_vat_amount'),
-      knex.raw('o.payment_service_provider AS payment_service_provider'),
-      knex.raw('o.pay_type AS pay_type'),
-      knex.raw('o.masked_card AS masked_card'),
-      knex.raw('o.total AS total'))
-  .where("o.invoice_id", ">", 0)
-  .andWhereRaw('CAST(DATE_ADD("1970-01-01", INTERVAL o.updatedAt/1000 SECOND) AS DATE) >= ?', [startDate])
-  .andWhereRaw('CAST(DATE_ADD("1970-01-01", INTERVAL o.updatedAt/1000 SECOND) AS DATE) <=?', [endDate])
-  .andWhere("o.client", client)        
-  .orderBy('o.paid', 'o.invoice_id');
+      knex.raw("c.id AS schedule_id"), 
+      knex.raw("CONCAT(c.date, 'T', c.`start_time`) AS start_datetime"),
+      knex.raw("CONCAT(c.date, 'T', c.`end_time`) AS end_datetime"),
+      knex.raw("ct.id AS class_type_id"),
+      knex.raw("ct.name AS class_type_name"),
+      knex.raw("ct.description AS class_type_desc"),
+      knex.raw("ct.updatedAt AS class_type_last_updated"),
+      knex.raw("r.id AS room_id"),
+      knex.raw("r.name AS room_name"),
+      knex.raw("r.updatedAt AS room_last_updated"),
+      knex.raw("c.seats AS total_spots"))
+  .where("c.client", partner_id)
+  .andWhere("b.id", venue_id)
+  .andWhereRaw("DATE BETWEEN ? AND ?", [start_date, end_date])
+  .orderBy('c.id');
 
-queryParameters.id = _.map(classPassTypesInRequestedPriceGroup, 'id')
-
-
-
-
-  console.log(venues);
+  console.log(schedules);
   
-  
-  console.log(venues.length, venues);
-  if (venues.length == 0) {
-    let fakeVenue = {};
-    fakeVenue.id = `client_${partner_id}_default_branch`;
-    fakeVenue.name = client.name;
-    fakeVenue.updatedAt = client.updatedAt;
-    venues.push(fakeVenue);
-  }
-
-  const countOfVenues = venues.length;
+  const countOfSchedules = schedules.length;
   let resData = {};
-  resData.venues = [];
+  resData.schedules = [];
   resData.pagination = {
     page: page,
     page_size: page_size,
-    total_pages: Math.ceil(countOfVenues / page_size)
+    total_pages: Math.ceil(countOfSchedules / page_size)
   };
 
-  if (page_size * (page - 1) < countOfVenues) {
+  if (page_size * (page - 1) < countOfSchedules) {
     // page number is valid
-    const numOfLastVenue = (page_size * page < countOfVenues) ? page_size * page : countOfVenues;
-    for (let i = (page_size * (page - 1)); i < numOfLastVenue; i++) {
-      let venue = {};
-      venue.partner_id = partner_id;
-      venue.venue_id = venues[i].id;
-      venue.venue_name = venues[i].name;
-      venue.address = {
-        address_line1: client.address_1,
-        address_line2: client.address_2,
-        city: client.city,
-        zip: client.zip_code,
-        country: client.country,
-      };
-      venue.phone = client.phone;
-      venue.email = client.email;
-      venue.website = client.website;
-      venue.last_updated = moment(venues[i].updatedAt).format();
-      resData.venues.push(venue);
-    }
+    // const numOfLastVenue = (page_size * page < countOfSchedules) ? page_size * page : countOfSchedules;
+    // for (let i = (page_size * (page - 1)); i < numOfLastVenue; i++) {
+    //   let schedule = {};
+    //   venue.partner_id = partner_id;
+    //   venue.venue_id = venues[i].id;
+    //   venue.venue_name = venues[i].name;
+    //   venue.address = {
+    //     address_line1: client.address_1,
+    //     address_line2: client.address_2,
+    //     city: client.city,
+    //     zip: client.zip_code,
+    //     country: client.country,
+    //   };
+    //   venue.phone = client.phone;
+    //   venue.email = client.email;
+    //   venue.website = client.website;
+    //   venue.last_updated = moment(venues[i].updatedAt).format();
+    //   resData.venues.push(venue);
+    // }
   } else {
     // page number is invalid
   }
