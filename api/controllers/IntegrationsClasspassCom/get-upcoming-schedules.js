30e7262e32b3b7661e159893f696ef5e61fd68a4
kum83 <momo821123@outlook.com>
2021-08-05 09:54:15 +0300

    f

--- api/controllers/IntegrationsClasspassCom/get-upcoming-schedules.js
+++ api/controllers/IntegrationsClasspassCom/get-upcoming-schedules.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const knex = require('../../services/knex')
 
 module.exports = async (req, res) => {
   const partner_id = req.params.partner_id;
@@ -19,6 +20,51 @@ module.exports = async (req, res) => {
 
   const venue = await Branch.findOne({client: partner_id, id: venue_id});
   if (!venue) return res.badRequest("Invalid venue_id");
+/*
+SELECT class.id AS schedule_id, CONCAT(class.date, 'T', class.`start_time`) AS start_datetime, CONCAT(class.date, 'T', class.`end_time`) AS end_datetime, class_type.id AS class_type_id,
+ class_type.name AS class_type_name, class_type.description AS class_type_desc, class_type.updatedAt AS class_type_last_updated, room.id AS room_id, room.name AS room_name, 
+ room.updatedAt AS room_last_updated, class.seats AS total_spots,   
+ 
+ FROM ((class LEFT JOIN room ON class.room=room.id) 
+ LEFT JOIN branch ON room.branch=branch.id) 
+ LEFT JOIN class_type ON class.class_type=class_type.id 
+ WHERE 
+ class.client=1 AND branch.id=1 AND DATE BETWEEN '2021-06-01' AND '2022-01-01'
+*/
+  const schedules = await knex({c: 'class'})
+  .leftJoin({r: 'room'}, 'r.id', 'c.room')
+  .leftJoin({b: 'branch'}, 'b.id', 'r.branch')
+  .leftJoin({ct: 'class_type'}, 'ct.id', 'c.class_type')
+  .innerJoin({oi: 'order_item'}, 'o.id', 'oi.order')
+  .select(
+      knex.raw('CAST(DATE_ADD("1970-01-01", INTERVAL o.paid/1000 SECOND) AS DATE) AS dd'), 
+      knex.raw('DATE_ADD("1970-01-01", INTERVAL FLOOR(o.paid/1000) SECOND) AS paid'), 
+      knex.raw('o.invoice_id AS invoice_id'),
+      knex.raw('o.user AS user_id'),
+      knex.raw('o.non_user_name AS non_user_name'),
+      knex.raw('o.non_user_email AS non_user_email'),
+      knex.raw('CONCAT(u.first_name, u.last_name) AS user_name'), 
+      knex.raw('u.email AS user_email'),
+      knex.raw('oi.name AS item_name'),
+      knex.raw('oi.item_type AS item_type'),
+      knex.raw('oi.item_id AS item_id'),
+      knex.raw('oi.count AS item_count'),
+      knex.raw('oi.item_price AS item_price'),
+      knex.raw('oi.total_price AS item_total_price'),
+      knex.raw('oi.vat_amount AS item_vat_amount'),
+      knex.raw('o.payment_service_provider AS payment_service_provider'),
+      knex.raw('o.pay_type AS pay_type'),
+      knex.raw('o.masked_card AS masked_card'),
+      knex.raw('o.total AS total'))
+  .where("o.invoice_id", ">", 0)
+  .andWhereRaw('CAST(DATE_ADD("1970-01-01", INTERVAL o.updatedAt/1000 SECOND) AS DATE) >= ?', [startDate])
+  .andWhereRaw('CAST(DATE_ADD("1970-01-01", INTERVAL o.updatedAt/1000 SECOND) AS DATE) <=?', [endDate])
+  .andWhere("o.client", client)        
+  .orderBy('o.paid', 'o.invoice_id');
+
+queryParameters.id = _.map(classPassTypesInRequestedPriceGroup, 'id')
+
+
 
 
   console.log(venues);
