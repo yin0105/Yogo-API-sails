1d9785f543f600d2cf6400e564c322c49bedef55
kum83 <momo821123@outlook.com>
2021-08-05 22:36:50 +0300

    f

--- api/controllers/IntegrationsClasspassCom/reservations.js
+++ api/controllers/IntegrationsClasspassCom/reservations.js
@@ -1,94 +1,97 @@
 const moment = require('moment');
 const knex = require('../../services/knex')
+const axios = require('axios').default;
 
 module.exports = async (req, res) => {
-  const partner_id = req.params.partner_id;
-  const venue_id = req.params.venue_id;
-  const page = req.query.page;
-  const page_size = req.query.page_size; 
-  const start_date = req.query.start_date;
-  const end_date = req.query.end_date;
+  console.log(req.params);
+  console.log(req.param);
+  console.log(req.body);
+  // const partner_id = req.params.id;
+  // const page = req.query.page;
+  // const page_size = req.query.page_size; 
+  // const venues = await Branch.find({client: partner_id});
+  // const clients = await knex({c: 'client'})
+  // .leftJoin({i: 'image'}, 'i.id', 'c.logo')
+  // .select(
+  //   knex.raw("c.name AS name"),
+  //   knex.raw("c.address_1 AS address_1"), 
+  //   knex.raw("c.address_2 AS address_2"),
+  //   knex.raw("c.city AS city"),
+  //   knex.raw("c.zip_code AS zip_code"),
+  //   knex.raw("c.country AS country"),
+  //   knex.raw("c.phone AS phone"),
+  //   knex.raw("c.email AS email"),
+  //   knex.raw("c.website AS website"),
+  //   knex.raw("i.original_width AS width"),
+  //   knex.raw("i.original_height AS height"),
+  //   knex.raw("i.filename AS uri"))
+  // .where('c.id', partner_id);
   
-  if (!page) return res.badRequest("Missing query 'page'");
-  if (!page_size) return res.badRequest("Missing query 'page_size'");
-  if (!start_date) return res.badRequest("Missing query 'start_date'");
-  if (!end_date) return res.badRequest("Missing query 'end_date'");
-
-  const client = await Client.findOne({id: partner_id});
-  if (!client) return res.badRequest("Invalid partner_id");
-
-  const venue = await Branch.findOne({client: partner_id, id: venue_id});
-  if (!venue) return res.badRequest("Invalid venue_id");
-/*
-SELECT , , , ,
- , , , , , 
- , ,   
- 
- FROM ((class LEFT JOIN room ON class.room=room.id) 
- LEFT JOIN branch ON room.branch=branch.id) 
- LEFT JOIN class_type ON class.class_type=class_type.id 
- WHERE 
- c.client=1 AND b.id=1 AND DATE BETWEEN '2021-06-01' AND '2022-01-01'
-*/
-  const schedules = await knex({c: 'class'})
-  .leftJoin({r: 'room'}, 'r.id', 'c.room')
-  .leftJoin({b: 'branch'}, 'b.id', 'r.branch')
-  .leftJoin({ct: 'class_type'}, 'ct.id', 'c.class_type')
-  .select(
-      knex.raw("c.id AS schedule_id"), 
-      knex.raw("CONCAT(c.date, 'T', c.`start_time`) AS start_datetime"),
-      knex.raw("CONCAT(c.date, 'T', c.`end_time`) AS end_datetime"),
-      knex.raw("ct.id AS class_type_id"),
-      knex.raw("ct.name AS class_type_name"),
-      knex.raw("ct.description AS class_type_description"),
-      knex.raw("ct.updatedAt AS class_type_last_updated"),
-      knex.raw("r.id AS room_id"),
-      knex.raw("r.name AS room_name"),
-      knex.raw("r.updatedAt AS room_last_updated"),
-      knex.raw("c.seats AS total_spots"))
-  .where("c.client", partner_id)
-  .andWhere("b.id", venue_id)
-  .andWhereRaw("DATE BETWEEN ? AND ?", [start_date, end_date])
-  .orderBy('c.id');
-
-  console.log(schedules);
+  // if (!page) return res.badRequest("Missing query 'page'");
+  // if (!page_size) return res.badRequest("Missing query 'page_size'");
+  // if (clients.length == 0) return res.badRequest("Invalid partner_id");  
   
-  const countOfSchedules = schedules.length;
-  let resData = {};
-  resData.schedules = [];
-  resData.pagination = {
-    page: page,
-    page_size: page_size,
-    total_pages: Math.ceil(countOfSchedules / page_size)
-  };
+  // if (venues.length == 0) {
+  //   let fakeVenue = {};
+  //   fakeVenue.id = `client_${partner_id}_default_branch`;
+  //   fakeVenue.name = clients[0].name;
+  //   fakeVenue.updatedAt = clients[0].updatedAt;
+  //   venues.push(fakeVenue);
+  // }
 
-  if (page_size * (page - 1) < countOfSchedules) {
-    // page number is valid
-    const numOfLastSchedule = (page_size * page < countOfSchedules) ? page_size * page : countOfSchedules;
-    for (let i = (page_size * (page - 1)); i < numOfLastSchedule; i++) {
-      let schedule = {};
-      schedule.id = schedules[i].schedule_id;
-      schedule.partner_id = partner_id;
-      schedule.venue_id = venue_id;
-      schedule.start_datetime = schedules[i].start_datetime;
-      schedule.end_datetime = schedules[i].end_datetime;
-      schedule.class = {
-        id: schedules[i].class_type_id,
-        name: schedules[i].class_type_name,
-        description: schedules[i].class_type_description,
-        last_updated: moment(schedules[i].class_type_last_updated).format(),
-      };
-      schedule.room = {
-        id: schedules[i].room_id,
-        name: schedules[i].room_name,
-        last_updated: moment(schedules[i].room_last_updated).format(),
-      };
-      schedule.total_spots = schedules[i].total_spots;
-      resData.schedules.push(schedule);
-    }
-  } else {
-    // page number is invalid
-  }
+  // const countOfVenues = venues.length;
+  // let resData = {};
+  // resData.venues = [];
+  // resData.pagination = {
+  //   page: page,
+  //   page_size: page_size,
+  //   total_pages: Math.ceil(countOfVenues / page_size)
+  // };
 
-  return res.json(resData);
+  // if (page_size * (page - 1) < countOfVenues) {
+  //   // page number is valid
+  //   const numOfLastVenue = (page_size * page < countOfVenues) ? page_size * page : countOfVenues;
+  //   for (let i = (page_size * (page - 1)); i < numOfLastVenue; i++) {
+  //     let venue = {};
+  //     venue.partner_id = partner_id;
+  //     venue.venue_id = venues[i].id;
+  //     venue.venue_name = venues[i].name;
+  //     venue.address = {
+  //       address_line1: clients[0].address_1,
+  //       address_line2: clients[0].address_2,
+  //       city: clients[0].city,
+  //       zip: clients[0].zip_code,
+  //       country: clients[0].country,
+  //     };
+  //     venue.phone = clients[0].phone;
+  //     venue.email = clients[0].email;
+  //     venue.website = clients[0].website;
+  //     venue.last_updated = moment(venues[i].updatedAt).format();
+
+  //     venue.images = [];
+  //     if (clients[0].uri) {
+  //       if (clients[0].width) {
+  //         venue.images.push({
+  //           width: clients[0].width,
+  //           height: clients[0].height,
+  //           url: `${sails.config.imgixServer}/${clients[0].uri}`,
+  //         });
+  //       } else {
+  //         let result = await axios.get(`${sails.config.imgixServer}/${clients[0].uri}?fm=json`)
+  //         venue.images.push({
+  //           width: result.data.PixelWidth,
+  //           height: result.data.PixelHeight,
+  //           url: `${sails.config.imgixServer}/${clients[0].uri}`,
+  //         });
+  //       }
+  //     }
+
+  //     resData.venues.push(venue);
+  //   }
+  // } else {
+  //   // page number is invalid
+  // }
+
+  // return res.json(resData);
+  return res.ok("asba");
 }
