02a826e35327777e06eaa950483cb230c67c3b8a
kum83 <momo821123@outlook.com>
2021-08-07 06:40:44 +0300

    f

--- api/controllers/IntegrationsClasspassCom/attendance.js
+++ api/controllers/IntegrationsClasspassCom/attendance.js
@@ -16,15 +16,18 @@ module.exports = async (req, res) => {
  */
   const attendances = await knex({cs: 'class_signup'})
   .leftJoin({c: 'class'}, 'c.id', 'cs.class')
+  .leftJoin({u: 'user'}, 'u.id', 'cs.user')
   .select(
     knex.raw("cs.classpass_com_reservation_id AS reservation_id"),
     knex.raw("cs.updatedAt AS updatedAt"),
-    knex.raw("cs.cancelled_at AS cancelled_at"), 
+    knex.raw("cs.cancelled_at AS cancelled_at"),
+    knex.raw("cs.checked_in AS checked_in"), 
     knex.raw("c.cancelled AS cancelled"),
     knex.raw("c.date AS date"),
     knex.raw("CONCAT(c.date, 'T', start_time) AS start_time"),
     knex.raw("c.end_time AS end_time"),
-    knex.raw("c.seats AS seats"))
+    knex.raw("c.seats AS seats"),
+    knex.raw("u.classpass_com_user_id AS cp_user_id"))
   .where('c.id', schedule_id);
   
   if (!page) return res.badRequest("Missing query 'page'");
@@ -43,31 +46,30 @@ module.exports = async (req, res) => {
     // page number is valid
     const numOfLastAttendance = (page_size * page < countOfAttendances) ? page_size * page : countOfAttendances;
     for (let i = (page_size * (page - 1)); i < numOfLastAttendance; i++) {
-      let attendance = {};
+      let attendance = {};      
+      attendance.cp_user_id = attendances[i].cp_user_id;      
       attendance.reservation_id = attendances[i].reservation_id;      
 
-      const class_signoff_deadline = await sails.helpers.clientSettings.find(partner_id, 'class_signoff_deadline');
-      const private_class_signup_deadline = await sails.helpers.clientSettings.find(partner_id, 'private_class_signup_deadline');      
       const start_window = moment(`${attendances[i].start_time}`);
-      const cancelled_window = moment(`${attendances[i].cancelled_at}`);
-      const late_cancel_window = attendances[i].seats == 1? start_window.subtract( private_class_signup_deadline, 'minutes') : start_window.subtract( class_signoff_deadline, 'minutes');
 
       // check status
       if (attendances[i].cancelled) {
         attendance.status = "CLASS_CANCELLED";
-      } else if (attendances[i].cancelled_at == 0) {
+      } else if (attendances[i].cancelled_at == 0) {        
         if (moment() < start_window) {
           attendance.status = "ENROLLED";
         } else {
-          attendance.status = "ATTENDED";
+          if (attendances[i].checked_in) {
+            attendance.status = "ATTENDED";
+          } else {
+            attendance.status = "MISSED";
+          }          
         }
       } else {
-        if ( cancelled_window < late_cancel_window) {
-          attendance.status = "CANCELLED";
-        } else if ( cancelled_window < start_window) {
+        if (attendances[i].late_cancel) {
           attendance.status = "LATE_CANCELLED";
         } else {
-          attendance.status = "MISSED";
+          attendance.status = "CANCELLED";
         }
       }
 
