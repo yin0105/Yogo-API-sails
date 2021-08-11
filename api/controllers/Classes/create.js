0c264fb3895c381fb1604b8b2fbf22a0301a0ffe
kum83 <momo821123@outlook.com>
2021-08-11 18:45:42 +0300

    f

--- api/controllers/Classes/create.js
+++ api/controllers/Classes/create.js
@@ -37,8 +37,9 @@ module.exports = {
     const createdClass = await Class.create(classData).fetch();
     console.log("createdClass = ", createdClass);
 
-    exits.success(createdClass);
+    // exits.success(createdClass);
 
+    let payload = {};
 
     const schedule = await knex.from({c: 'class'})
       .leftJoin({r: 'room'}, 'r.id', 'c.room')
@@ -62,83 +63,113 @@ module.exports = {
         knex.raw("c.classpass_com_all_seats_allowed AS classpass_com_all_seats_allowed"),
         knex.raw("c.classpass_com_number_of_seats_allowed AS classpass_com_number_of_seats_allowed"),
         knex.raw("c.seats AS seats"),
+        knex.raw("c.subtitle AS subtitle"),
         knex.raw("c.classpass_com_number_of_seats_allowed AS classpass_com_number_of_seats_allowed"))
       .where("c.id", createdClass.id);
       // .andWhere("b.id", venue_id)
       // .andWhereRaw("DATE BETWEEN ? AND ?", [start_date, end_date])
       // .orderBy('c.id');
 
+    console.log("schedule = ", schedule);
+
+    const teachers = await knex({c: 'class_teachers__user_teaching_classes'})
+      .leftJoin({u: 'user'}, 'u.id', 'c.user_teaching_classes')
+      .leftJoin({i: 'image'}, 'i.id', 'u.image')
+      .select(
+        knex.raw("c.id AS id"), 
+        knex.raw("u.first_name AS first_name"),
+        knex.raw("u.last_name AS last_name"),
+        knex.raw("u.updatedAt AS last_updated"),
+        knex.raw("i.original_width AS width"),
+        knex.raw("i.original_height AS height"),
+        knex.raw("i.filename AS uri"))
+      .where("c.class_teachers", schedule[0].id)
+      .orderBy('c.id');
+
+    payload.teachers = await Promise.all(teachers.map( async(teacher) => {
+      let images = [];
+      if (teacher.uri) {
+        if (teacher.width) {
+          images.push({
+            "width": teacher.width,
+            "height": teacher.height,
+            "url": `${sails.config.imgixServer}/${teacher.uri}`,
+          });
+          
+        } else {
+          const result = await axios.get(`${sails.config.imgixServer}/${teacher.uri}?fm=json`).catch(error => {console.log(error)})
+          images.push({
+            "width": result.data.PixelWidth,
+            "height": result.data.PixelHeight,
+            "url": `${sails.config.imgixServer}/${teacher.uri}`,
+          });
+        }
+      } 
+
+      return {
+        "id": teacher.id.toString(),
+        "first_name": teacher.first_name,
+        "last_name": teacher.last_name,
+        "last_updated": moment(teacher.last_updated).format(),
+        "images": images,
+      }
+      
+    }));
+
+    const classSignups = await knex({c: 'class_signup'})
+      .select( knex.raw("COUNT(*) AS signups") )
+      .where("c.class", schedule[0].schedule_id)
+      .andWhere("c.cancelled_at", 0);
+    
+    const actual_number_of_available_seats = schedule[0].seats - classSignups[0].signups;
+    const classpass_com_all_seats_allowed = schedule[0].classpass_com_all_seats_allowed;
+    const classpass_com_number_of_seats_allowed = schedule[0].classpass_com_number_of_seats_allowed;
+    const class_start = moment(schedule[0].start_datetime); // new Date(schedule[0].start_datetime);
+    const minsDiff = class_start.diff(new Date(), 'minutes');
+
+    payload.available_spots = actual_number_of_available_seats;
+    if (!classpass_com_all_seats_allowed) {
+      payload.available_spots = Math.min(actual_number_of_available_seats, classpass_com_number_of_seats_allowed);
+
+      if (classpass_com_release_all_seats_before_class_start) {
+        if (minsDiff > 0 && minsDiff < classpass_com_release_all_seats_minutes_before_class_start) {
+          payload.available_spots = actual_number_of_available_seats;
+        }
+      }
+    }
+
+    payload.late_cancel_window = schedule[0].seats == 1? class_start.subtract( private_class_signup_deadline, 'minutes') : class_start.subtract( class_signoff_deadline, 'minutes');
+    payload.bookable_window_starts = class_start.subtract( customer_can_sign_up_for_class_max_days_before_class, "days").local().format('YYYY-MM-DDTHH:mm:ss');
+    payload.bookable_window_ends = (schedule[0].seats == 1? class_start.subtract( private_class_signup_deadline, 'minutes') : class_start).local().format('YYYY-MM-DDTHH:mm:ss');
+
+    payload.id = createdClass.id;
+    payload.partner_id = classData.client;
+    payload.venue_id = schedule[0].venue_id;
+    payload.start_datetime = schedule[0].start_datetime;
+    payload.end_datetime = schedule[0].end_datetime;
+    payload.class = {
+        id: schedule[0].class_type_id.toString(),
+        name: schedule[0].class_type_name,
+        description: schedule[0].class_type_description,
+        last_updated: moment(schedule[0].class_type_last_updated).format(),
+    };
+    payload.schedule_name = schedule[0].subtitle;
+    payload.room = {
+      id: schedule[0].room_id.toString(),
+      name: schedule[0].room_name,
+      last_updated: moment(schedule[0].room_last_updated).format(),
+    };
+    payload.total_spots = schedule[0].total_spots;
+    payload.has_layout = false;
+    payload.is_cancelled = false;
+    payload.last_updated = moment(schedule[0].schedule_last_updated).format();
+
+    console.log("payload = ",payload);
+
     const resp = await sails.helpers.integrations.classpass.update.with({
       update:  {
         "type": "SCHEDULE",
-        "payload": {
-            "id": createdClass.id,
-            "partner_id": classData.client,
-            "venue_id": schedule[0].venue_id,
-            "start_datetime": schedule[0].start_datetime,
-            "end_datetime": schedule[0].end_datetime,
-            "class": {
-                "id": schedule[0].class_type_id.toString(),
-                "name": schedule[0].class_type_name,
-                "description": schedule[0].class_type_description,
-                "last_updated": moment(schedule[0].class_type_last_updated).format(),
-            },
-            "schedule_name": "AM Yoga",
-            "teachers": [
-                {
-                    "id": "uvt934",
-                    "first_name": "Jane",
-                    "last_name": "Doe",
-                    "display_name": "Jane D",
-                    "images": [
-                        {
-                            "width": 600,
-                            "height": 200,
-                            "url": "https://images.unsplash.com/photo-1490187291333-b4ece7bec2e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                        }
-                    ],
-                    "last_updated": "2019-01-02T20:30:38+00:00"
-                }
-            ],
-            "room": {
-                "id": schedule[0].room_id.toString(),
-                "name": schedule[0].room_name,
-                "last_updated": moment(schedule[0].room_last_updated).format(),
-            },
-            // "address": {
-            //     "address_line1": "123 Main St",
-            //     "address_line2": "Floor 1",
-            //     "city": "New York",
-            //     "state": "NY",
-            //     "zip": "10016",
-            //     "country": "US"
-            // },
-            // "coordinate": {
-            //     "latitude": 46.8697822,
-            //     "longitude": -113.995265
-            // },
-            "total_spots": schedules[i].total_spots,
-            "available_spots": 10,
-            "has_layout": false,
-            // "layout": [
-            //     {
-            //         "x": 1,
-            //         "y": 1,
-            //         "label": "A"
-            //     }
-            // ],
-            // "available_spot_labels": [
-            //     "A",
-            //     "B",
-            //     "C",
-            //     "D"
-            // ],
-            "bookable_window_starts": "2019-01-11T20:30:00",
-            "bookable_window_ends": "2019-01-12T20:30:00",
-            "late_cancel_window": "08:00",
-            "is_cancelled": false,
-            "last_updated": moment(schedules[i].schedule_last_updated).format()
-        }
+        "payload": payload
       },
     })
 
