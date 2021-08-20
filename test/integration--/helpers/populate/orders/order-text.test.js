3303408c6900d5f9b237e7119cff52a2d28f9f0b
kum83 <momo821123@outlook.com>
2021-08-21 02:47:20 +0300

    testing=> helpers: passed

--- test/integration--/helpers/populate/orders/order-text.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert')
-
-describe('helpers.populate.orders.order-text', () => {
-
-  it('should compose order text to use for PSPs', async () => {
-    // assert(false)
-  })
-
-})
