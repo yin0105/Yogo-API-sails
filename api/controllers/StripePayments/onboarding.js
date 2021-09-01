f641d65262a2eb973b18b62481c65895ab4ef977
kum83 <momo821123@outlook.com>
2021-09-01 12:26:53 +0300

    f

--- api/controllers/StripePayments/onboarding.js
+++ api/controllers/StripePayments/onboarding.js
@@ -32,10 +32,6 @@ module.exports = {
 
         let params = new URLSearchParams();
         params.append('type', 'standard');
-        // let data = querystring.stringify({type: 'standard'})
-        // const res = await axios.get('https://api.stripe.com/v1/accounts/acct_1JUXmvPYjWzBI4DI', {
-        //     headers: headers
-        // })
 
         // Create account
         let res = await axios.post('https://api.stripe.com/v1/accounts', params, {
@@ -46,6 +42,7 @@ module.exports = {
             return exits.onboardingFailed('Stripe onboarding is failed.');
         }
 
+        // return exits.onboardingFailed('Stripe onboarding is failed.');
         const accountId = res.data.id;
         
         // Create account link
@@ -60,45 +57,10 @@ module.exports = {
 
         const redirectURL = res.data.url;
 
-
         console.log("res = ", res)
-        // .then((response) => {
-        //     console.log("response = ", response);
-        //     accountId = response.id;
-        //     console.log("accountId = ", accountId);
-        //     // dispatch({
-        //     //     type: FOUND_USER,
-        //     //     data: response.data[0]
-        //     // })
-        // })
-        // .catch((error) => {
-        //     return exits.onboardingFailed('Stripe onboarding is failed.');
-        //     // dispatch({
-        //     //     type: ERROR_FINDING_USER
-        //     // })
-        // })
-
-        // let result = await axios.get(`${sails.config.imgixServer}/${clients[0].uri}?fm=json`)
+        console.log("redirectURL = ", redirectURL)
   
-    //   const order = await sails.helpers.order.createFromCart.with({
-    //     user: this.req.user.id,
-    //   })
-    //     .tolerate('paymentOptionGoneAway', () => {
-    //       return 'VALIDATION_ERROR'
-    //     })
-    //     .tolerate('userIsNotEligibleForCampaign', () => {
-    //       return 'VALIDATION_ERROR'
-    //     })
-  
-    //   if (order === 'VALIDATION_ERROR') {
-    //     exits.success('E_INVALID_CART_ITEM')
-    //   }
-  
-    //   const chargeSession = await sails.helpers.paymentProvider.reepay.createChargeSession.with({
-    //     order: order
-    //   })
-  
-        return exits.success(accountId)
+        return exits.success(redirectURL)
   
     },
   }
