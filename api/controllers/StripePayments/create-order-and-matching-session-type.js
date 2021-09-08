023023950773a2ec52c4bd0090950a67630fc938
kum83 <momo821123@outlook.com>
2021-09-08 21:48:58 +0300

    f

--- api/controllers/StripePayments/create-order-and-matching-session-type.js
+++ api/controllers/StripePayments/create-order-and-matching-session-type.js
@@ -15,6 +15,9 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
+    const secretKey = sails.config.paymentProviders.stripe.secretKey
+    const stripe = require("stripe")(secretKey);
+
     if (!await sails.helpers.can2('controller.StripePayments.create-order-and-matching-session-type', this.req)) {
       return exits.forbidden()
     }
@@ -44,27 +47,27 @@ module.exports = {
 
     // if (parseFloat(order.total) === 0) {
 
-      // const orderItems = await OrderItem.find({order: order.id})
-      // const orderHasRecurringPaymentItems = _.find(orderItems, {item_type: 'membership_type'})
-      // if (orderHasRecurringPaymentItems) {
+    //   const orderItems = await OrderItem.find({order: order.id})
+    //   const orderHasRecurringPaymentItems = _.find(orderItems, {item_type: 'membership_type'})
+    //   if (orderHasRecurringPaymentItems) {
 
-      //   const recurringSession = await sails.helpers.paymentProvider.stripe.createRecurringSession(this.req.user.id)
-      //   return exits.success({
-      //     status: 'RECURRING_SESSION_CREATED',
-      //     recurringSession: recurringSession,
-      //     order: order
-      //   })
+    //     // const recurringSession = await sails.helpers.paymentProvider.stripe.createRecurringSession(this.req.user.id)
+    //     return exits.success({
+    //       status: 'RECURRING_SESSION_CREATED',
+    //       // recurringSession: recurringSession,
+    //       order: order
+    //     })
 
-      // } else {
+    //   } else {
 
-      //   await sails.helpers.order.processPaymentSettled(order)
-      //   const processedOrder = await Order.findOne(order.id);
-      //   return exits.success({
-      //     status: 'ORDER_SETTLED',
-      //     order: processedOrder
-      //   })
+    //     await sails.helpers.order.processPaymentSettled(order)
+    //     const processedOrder = await Order.findOne(order.id);
+    //     return exits.success({
+    //       status: 'ORDER_SETTLED',
+    //       order: processedOrder
+    //     })
 
-      // }
+    //   }
 
     // }
 
@@ -72,8 +75,42 @@ module.exports = {
     //   order: order
     // })
 
+    // const paymentIntent = await stripe.paymentIntents.create({
+    //   amount: order.total,
+    //   currency: "usd"
+    // });
+
+    const clientSettings = await sails.helpers.clientSettings.find.with({
+      keys: ["plan_pay_as_you_grow_yogo_percentage", "payment_service_provider_stripe_account_id"],
+      client: this.req.client.id,
+      includeSecrets: true,
+    })
+    console.log("clientSettings = ", clientSettings)
+    const yogoPercent = parseFloat(clientSettings["plan_pay_as_you_grow_yogo_percentage"]);
+    const accountId = clientSettings["payment_service_provider_stripe_account_id"];
+    const feeAmount = parseInt((order.total - order.vat_amount) * 1.25 * yogoPercent);
+    const amount = parseInt(order.total * 100)
+    console.log("account id = ", accountId)
+    console.log("feeAmount = ", feeAmount)
+    console.log("amount = ", amount)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      payment_method_types: ['card'],
+      amount: amount,
+      currency: 'dkk',
+      application_fee_amount: feeAmount,
+    }, {
+      stripeAccount: accountId,
+    
+    });
+
+    console.log("secret = ", paymentIntent.client_secret)
+
     return exits.success({
       status: 'CHARGE_SESSION_CREATED',
+      clientSecret: paymentIntent.client_secret,
+      amount: amount,
+      accountId: accountId
       // chargeSession: chargeSession
     })
 
